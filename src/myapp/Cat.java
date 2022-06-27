df4327df7deba0d5dca4d3bc63ddf82c66bb26d8
Wongsakon Senewong N Ayotaya <wongsakon.s@ku.th>
2022-06-27 12:22:41 +0700

    Update Cat and add Dog

--- src/myapp/Cat.java
+++ src/myapp/Cat.java
@@ -1,4 +1,10 @@
 package myapp;
 
 public class Cat {
+    private String name;
+    private String color;
+
+    public void getStatus(){
+        System.out.println("Name : " + name + " Color : " + color);
+    }
 }
