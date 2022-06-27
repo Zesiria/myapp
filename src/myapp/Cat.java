2aef9bbc81372845c2883c9718032bc758451047
Wongsakon Senewong N Ayotaya <wongsakon.s@ku.th>
2022-06-27 12:28:52 +0700

    bring back src/myapp/Cat.java from commit be56535

--- src/myapp/Cat.java
+++ src/myapp/Cat.java
@@ -1,10 +1,4 @@
 package myapp;
 
 public class Cat {
-    private String name;
-    private String color;
-
-    public void getStatus(){
-        System.out.println("Name : " + name + " Color : " + color);
-    }
 }
